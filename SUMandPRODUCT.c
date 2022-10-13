e47b36e74b13792c523003899fea357b52f343ac
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2022-10-13 12:55:04 +0530

    My_Changes

---
+++ SUMandPRODUCT.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+	 { 
+		 int a,b,c,d;
+	         printf("enter two numbers");
+		 scanf("%d %d",&a,&b);
+		 c=a+b;
+		 d=a*b;
+		 printf("Sum : %d\n",c);
+		 printf("Product : %d \n",d);
+		 return 0;
+	 }
